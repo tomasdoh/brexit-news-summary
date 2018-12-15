ac0313dd0cd196b46e707084bd20b5f08dffc759
Tomas Doherty <tomasdoh1@gmail.com>
2018-12-15 15:09:55 +0000

    API connection working - headlines rendering
    Page now showing headlines from most recent articles. Connection made 
    with fetch(). Still to re-factor into MVC with tests. API key in .env.

---
+++ app.js
@@ -0,0 +1,28 @@
+
+function createNode(element) {
+    return document.createElement(element); // Create the type of element you pass in the parameters
+  }
+
+function append(parent, el) {
+  return parent.appendChild(el); // Append the second parameter(element) to the first one
+}
+
+const url = `https://content.guardianapis.com/search?api-key=${GUARDIAN_API_KEY}`;
+
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let articles = data.response.results;
+      articles.forEach(function(article) {
+        let headline = article.webTitle;
+        let para = createNode('p');
+        para.innerHTML = headline;
+        append(document.body, para);
+      })
+    })
+    .catch(function(error) {
+      console.log(error)
+    });
